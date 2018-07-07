75dba88dc13d2eca6be1dffe951cc9d261359901
Isaac <Isaacsunoo@gmail.com>
2018-07-07 16:20:40 -0600

    Revised

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -6,8 +6,8 @@ public class MakeChange {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int quarters = 25, dimes = 10, nickels = 5, pennies = 1;
-		int hundreds = 100, fifties = 50, twenties = 20, tens = 10, fives = 5, ones = 1;
+		int quarters, dimes, nickels, pennies;
+		int hundreds, fifties, twenties, tens, fives, ones;
 
 		System.out.print("Please tell me the asking price of the item: \t");
 		double askingPrice = sc.nextDouble();
@@ -15,12 +15,12 @@ public class MakeChange {
 		double amountTendered = sc.nextDouble();
 
 		if (askingPrice > amountTendered) {
-			System.out.println("You have underpaid and money is still owed.");
+			System.err.println("You have underpaid and money is still owed.");
 		} else if (askingPrice == amountTendered) {
 			System.out.println("The amount recieved is the exact amount necessary.");
 		}
+		
 		double changeInitial = amountTendered - askingPrice;
-
 		double change = Math.round(changeInitial * 100.0) / 100.0;
 
 		System.out.println("Your change due is : " + change);
@@ -46,29 +46,35 @@ public class MakeChange {
 		do {
 			if (hundreds != 0) {
 				System.out.println("Hundreds: " + hundreds);
-				System.out.println();
 			}
 			if (fifties != 0) {
 				System.out.println("Fifties: " + fifties);
-				System.out.println();
 			}
 			if (twenties != 0) {
 				System.out.println("Twenties: " + twenties);
-				System.out.println();
 			}
 			if (tens != 0) {
 				System.out.println("Tens: " + tens);
-				System.out.println();
 			}
 			if (fives != 0) {
 				System.out.println("Fives: " + fives);
-				System.out.println();
 			}
 			if (ones != 0) {
-				System.out.println("Ones: ");
-				System.out.println();
+				System.out.println("Ones: " + ones);
 			}
-			if ()
+			if (quarters != 0) {
+				System.out.println("Quarters: " + quarters);
+			}
+			if (dimes != 0) {
+				System.out.println("Dimes: " + dimes);
+			}
+			if (nickels != 0) {
+				System.out.println("Nickels: " + nickels);
+			}
+			if (pennies != 0) {
+				System.out.println("Pennies: " + pennies);
+			}
+			while 
 		}
 		
 
